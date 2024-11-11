59e5f516ff2092f5bc58a85221d3c5c3eea6dec0
Rumia <lemmings1010@yahoo.co.kp>
2024-11-11 13:56:37 +0900

    GameController作成

--- Assets/Scripts/AzarashiController.cs
+++ Assets/Scripts/AzarashiController.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AzarashiController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     Animator animator;
     float angle;
+    bool isDead;
 
     public float maxHeight;
     public float flapVelocity;
     public float relativeVelocityX;
     public GameObject sprite;
+
+    public bool IsDead(){
+        return isDead;
+    }
 
     void Awake(){
         rb2d=GetComponent<Rigidbody2D>();
@@ -26,16 +32,36 @@ public class AzarashiController : MonoBehaviour
         }
 
         ApplyAngle();
-        animator.SetBool("flap", angle>=0.0f);
+        animator.SetBool("flap", angle>=0.0f && !isDead);
     }
 
     public void Flap(){
+        if(isDead) return;
+        if(rb2d.isKinematic) return;
         rb2d.velocity=new Vector2(0.0f, flapVelocity);
     }
 
     void ApplyAngle(){
-        float targetAngle=Mathf.Atan2(rb2d.velocity.y, relativeVelocityX)*Mathf.Rad2Deg;
+
+        float targetAngle;
+
+        if(isDead){
+            targetAngle=180.0f;
+        }else{
+            targetAngle=Mathf.Atan2(rb2d.velocity.y, relativeVelocityX)*Mathf.Rad2Deg;
+        }
+        
         angle =Mathf.Lerp(angle, targetAngle, Time.deltaTime*10.0f);
         sprite.transform.localRotation=Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(isDead) return;
+
+        isDead = true;
+    }
+
+    public void SetSteerActive(bool active){
+        rb2d.isKinematic = !active;
     }
 }
