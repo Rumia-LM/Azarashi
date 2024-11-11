0b2aa8fce352d2a75290bb156028f1d3d9b74369
Rumia <lemmings1010@yahoo.co.kp>
2024-11-11 09:50:16 +0900

    initial commit

---
+++ Assets/ScrollObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed=1.0f;
+    public float startPosition;
+    public float endPosition;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-1*speed*Time.deltaTime,0,0);
+        if(transform.position.x<=endPosition) ScrollEnd();     
+    }
+
+    void ScrollEnd(){
+        float diff = transform.position.x-endPosition;
+        Vector3 restartPosotion=transform.position;
+        restartPosotion.x=startPosition+diff;
+    }
+}
